ea81f0844c6c6731c9f63b848501d0166ef49c80
Raul Kele <raulkeleblk@gmail.com>
2022-09-27 12:17:46 +0300

    remove unused mock data
    Signed-off-by: Raul Kele <raulkeleblk@gmail.com>

--- src/utilities/mockData.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable max-len */
-const mockData = {
-  loremIpsum:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. At quam nulla euismod mus sed. Gravida ornare tortor diam nullam donec eget. Sed et cursus tristique leo arcu eget sed. Sit neque morbi praesent tellus duis lectus orci.\n Laoreet sodales aenean libero pharetra tellus. Nisl blandit quis lorem platea. Mauris id neque nec blandit ipsum aliquet venenatis egestas sed. Tempus felis sed aliquam proin aliquet. Elementum egestas sagittis nibh orci, varius interdum pretium risus. Nullam eu id tempus faucibus nullam purus nibh mi.\n Tincidunt pretium vehicula metus a dui est proin. Ullamcorper vitae pulvinar diam habitant a, tellus pellentesque consectetur et. Vel, pellentesque tellus ultrices non molestie. Eu, est dignissim sit vivamus. At orci, urna rhoncus sed ultrices. Dui elit dui vestibulum ipsum sed morbi pellentesque sed lacus.'
-};
-
-export default mockData;
