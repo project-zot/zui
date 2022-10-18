95495ccc23f4b718db599a22b4036d366aacc357
Raul Kele <raulkeleblk@gmail.com>
2022-10-18 12:52:30 +0300

    patch: Added arrow to digest dropdown Updated md parser config
    Signed-off-by: Raul Kele <raulkeleblk@gmail.com>

--- src/__tests__/RepoPage/Tags.test.js
+++ src/__tests__/RepoPage/Tags.test.js
@@ -24,13 +24,15 @@ const mockedTagsData = [
 ];
 
 describe('Tags component', () => {
-  it('should open and close details dropdown for tags', () => {
+  it('should open and close details dropdown for tags', async () => {
     render(<Tags tags={mockedTagsData} />);
-    const openBtn = screen.getByText(/see digest/i);
+    const openBtn = screen.getByText(/digest/i);
     fireEvent.click(openBtn);
-    expect(screen.queryByText(/see digest/i)).not.toBeInTheDocument();
-    expect(screen.getByText(/hide digest/i)).toBeInTheDocument();
+    expect(screen.getByText(/OS\/ARCH/i)).toBeInTheDocument();
+    fireEvent.click(openBtn);
+    await waitFor(() => expect(screen.queryByText(/OS\/ARCH/i)).not.toBeInTheDocument());
   });
+
   it('should navigate to tag page details when tag is clicked', async () => {
     render(<Tags tags={mockedTagsData} />);
     const tagLink = await screen.findByText('latest');
