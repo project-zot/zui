0bdf816bdc0e69a6ed0a064b8367db164a756e4d
Raul Kele <raulkeleblk@gmail.com>
2022-11-21 12:53:25 +0200

    fix: Provide p tag override to avoid md parsing errors
    Signed-off-by: Raul Kele <raulkeleblk@gmail.com>

--- src/utilities/MarkdowntojsxWrapper.jsx
+++ src/utilities/MarkdowntojsxWrapper.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Markdown from 'markdown-to-jsx';
 
+// used to override p tags in compiled md
+const ParagraphOverride = ({ children, ...props }) => <span {...props}>{children}</span>;
+
 const MarkdownWrapper = (props) => {
   const { children, options } = props;
   return (
     <Markdown
       {...props}
-      options={{ ...options, disableParsingRawHTML: true, overrides: { a: { props: { target: '_blank' } } } }}
+      options={{
+        ...options,
+        disableParsingRawHTML: true,
+        overrides: { p: { component: ParagraphOverride }, a: { props: { target: '_blank' } } }
+      }}
     >
       {children}
     </Markdown>
