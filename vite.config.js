597908d9050339ed9b32391dc3306af442ccbf38
Asgeir Nilsen <asgeir@twingine.no>
2025-06-18 07:37:49 +0200

    Fix image loading CSP and clean up vite.config.js prettier warnings.
    Signed-off-by: Asgeir Nilsen <asgeir@twingine.no>

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     react({
-      include: "**/*.{jsx,js}"
+      include: '**/*.{jsx,js}'
     })
   ],
   publicDir: 'public',
   resolve: {
     alias: {
-      'api': path.resolve(__dirname, './src/api.js'),
-      'utilities': path.resolve(__dirname, './src/utilities'),
-      'components': path.resolve(__dirname, './src/components'),
-      'pages': path.resolve(__dirname, './src/pages'),
-      'assets': path.resolve(__dirname, './src/assets'),
-      '__mocks__': path.resolve(__dirname, './src/__mocks__'),
-      'host': path.resolve(__dirname, './src/host.js'),
-      'session': path.resolve(__dirname, './src/session.js')
+      api: path.resolve(__dirname, './src/api.js'),
+      utilities: path.resolve(__dirname, './src/utilities'),
+      components: path.resolve(__dirname, './src/components'),
+      pages: path.resolve(__dirname, './src/pages'),
+      assets: path.resolve(__dirname, './src/assets'),
+      __mocks__: path.resolve(__dirname, './src/__mocks__'),
+      host: path.resolve(__dirname, './src/host.js'),
+      session: path.resolve(__dirname, './src/session.js')
     }
   },
   build: {
@@ -30,4 +30,4 @@ export default defineConfig({
     include: /src\/.*\.[jt]sx?$/,
     exclude: []
   }
-})
+});
