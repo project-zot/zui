74abae129b76244ba5772ba08418cccbed41409f
Asgeir Nilsen <asgeir@twingine.no>
2025-11-07 11:03:32 +0100

    Update Babel config as recommended by Copilot.
    Signed-off-by: Asgeir Nilsen <asgeir@twingine.no>

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,41 @@
-export const presets = [
-  [
-    '@babel/preset-env',
-    {
-      targets: {
-        node: 'current'
+export default {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        },
+        modules: false
       }
-    }
+    ],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic'
+      }
+    ]
   ],
-  [
-    '@babel/preset-react',
-    {
-      runtime: 'automatic'
+  plugins: ['@babel/plugin-proposal-private-property-in-object'],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current'
+            },
+            modules: 'commonjs'
+          }
+        ],
+        [
+          '@babel/preset-react',
+          {
+            runtime: 'automatic'
+          }
+        ]
+      ]
     }
-  ]
-];
-export const plugins = ['@babel/plugin-proposal-private-property-in-object'];
+  }
+};
