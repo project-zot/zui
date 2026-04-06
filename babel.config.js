1c8e5ef68b2898703f0a937de67ac60aff3e356b
Ramkumar Chinchani <45800463+rchincha@users.noreply.github.com>
2026-04-05 23:11:51 -0700

    chore: fix snyk alerts (#511)
    * chore: fix snyk alerts
    
    Signed-off-by: Ramkumar Chinchani <rchincha.dev@gmail.com>
    
    * ci: update trivy version
    
    Signed-off-by: Ramkumar Chinchani <rchincha.dev@gmail.com>
    
    ---------
    
    Signed-off-by: Ramkumar Chinchani <rchincha.dev@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -16,7 +16,7 @@ export default {
       }
     ]
   ],
-  plugins: ['@babel/plugin-proposal-private-property-in-object'],
+  plugins: ['@babel/plugin-transform-private-property-in-object'],
   env: {
     test: {
       presets: [
